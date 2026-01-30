b69daa24e20d5ccbb6d1c9887d8d96075dd6e82e
Meron Oumer <meron.oumer2005@gmail.com>
2026-01-29 23:58:54 -0500

    Time: 16 ms (72.83%) | Memory: 12.7 MB (69.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -19,9 +19,46 @@ class Solution(object):
 
             crosschecking str and hashsets for charachters and similarities
         """
-        new_s = "".join(sorted(s))
-        new_t = "".join(sorted(t))
-        return new_s == new_t
+
+        # simplest way 
+        # new_s = "".join(sorted(s))
+        # new_t = "".join(sorted(t))
+        # return new_s == new_t
+
+        # by hashing 
+
+
+
+        s_seen = {}
+        s_counter = 0
+        t_seen = {}
+        t_counter = 0
+        for s_char in s:
+            if s_char not in s_seen:
+                s_counter = 1
+                s_seen[s_char]= s_counter
+            else:
+                s_counter = s_seen[s_char]
+                s_counter +=1
+                s_seen[s_char]= s_counter
+        for t_char in t:
+            if t_char not in t_seen:
+                t_counter = 1
+                t_seen[t_char]= t_counter
+            else:
+                t_counter = t_seen[t_char]
+                t_counter +=1
+                t_seen[t_char]= t_counter
+
+        print(dict(sorted(s_seen.items())))
+        print(dict(sorted(t_seen.items())))
+
+        print(s_seen == t_seen)
+
+        return s_seen == t_seen
+
+
+
 
 
         
