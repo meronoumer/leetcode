71719c8816feb5d98e008743d3856e0b32635221
Meron Oumer <meron.oumer2005@gmail.com>
2026-05-24 14:12:43 -0400

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.11%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,29 +1,22 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        """
-        input = arr w x nums
-        output = indices of numbers that add to give us the target
-
-        iterate through arr
-        have two pointers
-        whenever two pointers add up to a target then end loop and return the indices of those two vals
-
-        for time
-            - no nested for loop
-                - initialize the index of both ptrs and update as we go along 
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
 
         """
-        set_indices = set()
-        dict = {}
-        for num_ind in range(len(nums)):
-            if target-nums[num_ind] in dict:
-                return [dict[target-nums[num_ind]],num_ind]
+        input = array of numbers
+        number we are looking for 
+
+        output = 
+            - indices of the two addennds
+
+            -
+
+        """
+        seen = {}
+        for ind,num in enumerate(nums):
+            sub = target-num
+            if sub in seen:
+                return seen[sub],ind
             else:
-                dict[nums[num_ind]]= num_ind
-
+                seen[num]=ind
+        
