38ea65fa06ca8e82d6c17f00a6bb1af47eb06064
Meron Oumer <meron.oumer2005@gmail.com>
2026-05-24 13:03:24 -0400

    Time: 27 ms (5.30%) | Memory: 19.9 MB (21.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,4 +4,33 @@ class Solution:
         s = "".join(sorted(s))
         t = "".join(sorted(t))
 
-        return s==t
+        """
+        input = strings 1 and 2 
+        output = boolean
+
+
+        t
+            - if anagram = if they have the same letters and same number of letters
+                            + order doesnt matter
+        check by 
+            - storing = > k,v = >hashmap
+            - compare the two hashmaps to eachother 
+            
+        """
+        s_dict = {}
+        for char in s:
+            if char not in s_dict:
+                s_dict[char]=1
+            else:
+                s_dict[char]+=1
+        
+        t_dict = {}
+        for char in t:
+            if char not in t_dict:
+                t_dict[char]=1
+            else:
+                t_dict[char]+=1
+
+
+        return s_dict ==t_dict
+
