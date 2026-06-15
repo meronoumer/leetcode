aca05cb2e72c3800a12d3e4183b46389bf7dddb4
Meron Oumer <meron.oumer2005@gmail.com>
2026-06-15 12:59:16 -0400

    Time: 15 ms (71.89%) | Memory: 12.6 MB (92.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,71 +1,47 @@
 class Solution(object):
     def isPalindrome(self, s):
         """
-        :type s: str
-        :rtype: bool
+        - lowercasing 
+        - removing all non-alpha nums
+
+        frst letter = last letter
+        next = next 
+        etc....
 
 
-         - input 
+        2 ptr
 
-         - output
+        have something ptg at start of string
+        have one at the end
 
-         bool
-            -true
-                reads the same forward and backward
-            -false
-                -o reads the same forward and backward
+        once not equal 
+            - terminate 
+                return false
+        if equal
+            - keep going 
+                - iterating 
+                    start <end  = indices
         
 
-        simplest 
-            - keep the first string 
-            - reverse the secod string 
-                - splicig 
-            return f they're equivalent 
-
-            - o(n)
-                - iter remove any no al num   
-                - lower
-
-        
-        two pointer
-            have an index ptr at start and end of my string 
-
-                check f equal - iteratively
-                    not 
-                        return false
-                
-                
-                return true
-
-                const
-                    - start< end
-
-                 
-                 s .lower()
-                 = while loop
-                  - if char at left is not alphanumeric 
-                    tehn increment by one
-                    or at right is not alpha numeric :
-                    decremeent by one
         """
         s = s.lower()
+        left = 0
+        right = len(s)-1
+        print
+        while left<right:
+            
+            
+            if not s[left].isalnum():
+                left+=1
+                continue
+            elif not s[right].isalnum():
+                right-=1
+                continue
 
-        start = 0
-        end = len(s)-1
-        while start<end:
-            if not s[start].isalnum():
-                start+=1
-                continue
-            elif not s[end].isalnum():
-                end-=1
-                continue
-            if s[start]!=s[end]:
+            if s[left]!=s[right]:
                 return False
+
             else:
-                start += 1
-                end -= 1
+                left+=1
+                right-=1
         return True
-        
-
-
-
