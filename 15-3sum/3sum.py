4d4c598e05bfca1009dc4349ac12f013ccf6919f
Meron Oumer <meron.oumer2005@gmail.com>
2026-06-15 18:30:07 -0400

    Time: 776 ms (37.24%) | Memory: 18.2 MB (78.04%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,8 +1,19 @@
 class Solution(object):
     def threeSum(self, nums):
+        """
+
+        init larg arr
+        two pointers
+        way of tracking 3rd 
+            = iterate thru 
+                - for loop
+                    for each number in i we could add it to a set and then check the sum of the numbers to the left ad right 
+                    if the number at right + th enumber at left is 
+
+
+        """
         large_arr = []
         nums = sorted(nums)
-
         for i, num in enumerate(nums):
             # FIX 1: Skip duplicate values for 'i' to avoid duplicate triplets
             if i > 0 and nums[i] == nums[i - 1]:
@@ -11,25 +22,18 @@ class Solution(object):
             left = i + 1
             right = len(nums) - 1
             
-            while left < right:
-                total_sum = nums[i] + nums[left] + nums[right]
-                
-                if total_sum == 0:
-                    # FIX 2: Append directly to large_arr
-                    large_arr.append([nums[i], nums[left], nums[right]])
-                    
-                    left += 1
-                    right -= 1
-                    
-                    # FIX 3: Skip duplicate values for 'left' and 'right'
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                        
-                elif total_sum > 0:
-                    right -= 1
-                elif total_sum < 0:
-                    left += 1
-
+            while left<right:
+                total_sum = nums[i]+nums[left]+nums[right]
+                if total_sum==0:
+                    large_arr.append([nums[i],nums[left],nums[right]])
+                    left+=1
+                    right-=1
+                    while left<right and nums[left]==nums[left-1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                elif total_sum>0:
+                    right-=1
+                elif total_sum<0:
+                    left+=1
         return large_arr
