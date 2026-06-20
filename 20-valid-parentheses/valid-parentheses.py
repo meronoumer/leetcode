47fd19e5e5d035c63445fab4a4a72fa619a69989
Meron Oumer <meron.oumer2005@gmail.com>
2026-06-19 23:30:12 -0400

    Time: 3 ms (74.55%) | Memory: 12.5 MB (37.57%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def isValid(self, s):
+
+        """
+        input 
+            = string 
+                with different charcters 
+                if characters close each other then 
+
+        """
+        stack = []
+
+        openers = ["(","{","["]
+        # str = list(s)
+        # Keys are closers, values are openers
+        maps = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+        for char in s:
+            if char in openers:
+                stack.append(char)
+            else:
+                if len(stack)==0:
+                    return False
+                if stack[-1]==maps[char]:
+                    stack.pop()
+                elif stack[-1]!=maps[char]:
+                    return False
+        
+        if len(stack)==0:
+            return True
+        else:
+            return False
