c08c0b817538105e633f249657bfa7472be8d2f2
Meron Oumer <meron.oumer2005@gmail.com>
2026-07-01 19:02:35 -0400

    Time: 7 ms (25.31%) | Memory: 20.2 MB (77.92%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: Optional[TreeNode]
+        :type k: int
+        :rtype: int
+        """
+        arr = []
+
+        def inorder(node):
+            if not node:
+                return None
+            inorder(node.left)
+            arr.append(node.val)
+            inorder(node.right)
+        inorder(root)
+        return arr[k-1]   
+
+
+        
