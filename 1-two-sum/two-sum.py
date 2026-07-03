f8d3f522d2afa366c0992622e3c1efd250ce4396
Meron Oumer <meron.oumer2005@gmail.com>
2026-07-03 19:57:59 -0400

    Time: 0 ms (100.00%) | Memory: 12.8 MB (94.69%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,31 +1,40 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+
+
+
+
+        input - array of numbers - a number - targ. that two nums could sum to 
+
+        output 
+            - array of indidces of where these addends are located
+        goal 
+            - use dict 
+                - tracking for two things
+                - num itself
+                - indices - where it is
+            - traverse nums
+                - add to dict + location
+            - now use target and iteratively calculate the diff between all keys and target 
+                if diff in dic 
+                    - yes - ret ind of diff and 
+                
+                
 
         """
-        input = array of numbers
-        number we are looking for 
-
-        output = 
-            - indices of the two addennds
-
-        
-        ind, num
-             = hashmap
-        
-        
-        
-
-
-            -
-
-        """
         seen = {}
-        for ind,num in enumerate(nums):
-            sub = target-num
-            if sub in seen:
-                return seen[sub],ind
-            else:
-                seen[num]=ind
-        return {}
+        for index,num in enumerate(nums):
+            diff = target-num
+            if diff in seen:
+                return [seen[diff],index]
+            seen[num]=index
+
+
+
+
+
         
