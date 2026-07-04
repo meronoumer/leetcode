5a78e4fde8d7b6c7b01f1d26856711ab624e492f
Meron Oumer <meron.oumer2005@gmail.com>
2026-07-03 22:42:47 -0400

    Time: 5 ms (87.94%) | Memory: 13.9 MB (97.16%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,48 +4,39 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
+
+
+
+        input 
+            - nums - array of numbers
+            - k = number of most frequent elements that appear
+
+        output 
+            - arr of k most freq app nums
+        goal 
+             - init a dict 
+                - looking at two things 
+                    - num
+                    - no of times they appear
+                js update dct on freq and 
+
+                how to access k 
+                    - how to access the most freqently app ones
+                    can i arrange them n the order of 
+                     ---sorted_dict_desc = dict(sorted(my_dict.items(), key=lambda x: x[1], reverse=True))
+    ret that values by slicing to k+1
         """
-        dict = {}
+
+        seen = {}
+
+
         for num in nums:
-
-            if num in dict:
-                
-                dict[num]+=1 
-                   
+            if num in seen:
+                seen[num]+=1
             else:
+                seen[num]=1
+        sorted_keys = sorted(seen.keys(), key=lambda x: seen[x], reverse=True)
 
-                dict[num]=1
-            
-        
-        
-
-
-
-        # val = -float("inf")
-        # val_arr = []
-        # for key,freq in dict.items():
-        #     # if freq>val:
-        #     val_arr.append([key,freq])
-        # val_arr.sort(reverse = True)
-
-                # val_arr[0]=key
-            # if dict[val_new] < val:
-            #     val_arr.append(key)
-
-        # build array in descending order
-        #  then finally return truncated version
-        val_arr = []
-        for key, count in dict.items():
-            val_arr.append([count, key])  
-
-        val_arr.sort(reverse=True)
-
-        res = []
-        for i in range(k):
-            res.append(val_arr[i][1])
-
-        return res
-
-
+        return sorted_keys[:k]
 
         
